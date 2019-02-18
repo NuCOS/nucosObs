69577754ed5a2ca5a161dc52cfafeb4e9f572eee
oliver <oliver.braun@nucos.de>
2019-02-18 08:25:04 +0100

    debug aiohht example

---
+++ examples/aiohttpServer.py
@@ -0,0 +1,126 @@
+
+from aiohttp import web
+import asyncio as aio
+import ssl 
+try:
+    import simplejson as json
+except:
+    import json
+from nucosCR import hexdigest_n
+
+from nucosObs import main_loop, loop, debug
+from nucosObs.observer import Observer, inThread, broadcast
+from nucosObs.observable import Observable
+from nucosObs.aiohttpWebsocketInterface import AiohttpWebsocketInterface
+from logger import Logger
+logger = Logger("Logger")
+logger.level("INFO")
+
+
+
+debug.append(True)
+
+messageBroker = Observable()
+user = Observable()
+server_cert = 'server.crt'
+server_key = 'server.key'
+client_certs = 'client.crt'
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER) # ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.verify_mode = ssl.CERT_REQUIRED
+context.load_cert_chain(certfile=server_cert, keyfile=server_key)
+context.load_verify_locations(cafile=client_certs)
+# for testing puprose ...
+users = {'test-user': '7d0bff4edd541774e07692b71c8f1af082682d6f1b158e1c29e156d1838c624b'}
+
+def authenticate(id_, user, nonce, challenge):
+    # pre = hexdigest_n('test123', 100)
+    # print("PRE local", pre)
+    digest = hexdigest_n(users[user] + nonce, 100)
+    # print(digest, challenge, pre)
+    return digest == challenge
+
+def check_session_key(key, user):
+    return True
+
+class Authenticator():
+    def __init__(self):
+        self.approved = False
+
+    async def startAuth(self, msg, wsi, nonce):
+        inp = json.loads(msg)
+        args = inp["args"]
+        try:
+            user, challenge, id_ = args["user"], args["challenge"], args["id"]
+        except:
+            return None, user
+        if debug[-1]:
+            print("start auth", msg)
+        if authenticate(id_, user, nonce, challenge):
+            context = {"name": "finalizeAuth",
+                       "args": {"authenticated": True, "id": id_},
+                       "action": "authenticated"}
+            await wsi.send(json.dumps(context))
+            if debug[-1]:
+                print("Authenticate accepted of user %s" % user)
+            return id_, user
+        else:
+            context = {"name": "finalizeAuth",
+                       "args": {"authenticated": False, "id": id_},
+                       "action": "authenticated"}
+            await wsi.send(json.dumps(context))
+            if debug[-1]:
+                print("Authenticate refused")
+            return None, user
+
+
+class WebsocketObserver(Observer):
+    def __init__(self, name, observable, wsi, concurrent=[]):
+        super(WebsocketObserver, self).__init__(name, observable, concurrent)
+        self.wsi = wsi
+
+    async def send(self, user, session_key, msg):
+        if isinstance(msg, dict):
+            msg = json.dumps(msg)
+        if check_session_key(session_key, user):
+            await self.wsi.send(msg, user)
+            logger.log(lvl="INFO", msg="message send: %s %s" % (user, msg))
+
+    def parse(self, item):
+        logger.log(lvl="INFO", msg="message received: %s" % item)
+        try:
+            item = json.loads(item)
+        except:
+            logger.log("WARNING", msg="no json found ... do nothing")
+            return False, None, None
+        return super(WebsocketObserver, self).parse(item)
+
+    async def addUser(self, user, session_key, new_user, secret):
+        logger.log(lvl="INFO", msg="addUser called %s %s" % (user, new_user))
+        if user == "admin":
+            logger.log(lvl="INFO", msg="new user added %s" % new_user)
+            cache.set(new_user, secret)
+        msg = {
+               "name": "logger",
+               "args": {"log": "addUser called"}
+              }
+        await self.send(user, session_key, msg)
+    
+
+
+app = web.Application()
+wsi = AiohttpWebsocketInterface(app, messageBroker, doAuth=True, closeOnClientQuit=False, authenticator=Authenticator(), ssl=context)
+obs = WebsocketObserver("WSO", messageBroker, wsi)
+
+async def startApp():
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', 8081)
+    await site.start()
+
+
+#start the main loop with Interfaces
+main_loop([startApp(), ])
+
+
+
---
+++ nucosObs/aiohttpWebsocketInterface.py
@@ -0,0 +1,152 @@
+import asyncio as aio
+from aiohttp import web
+import aiohttp
+try:
+    import simplejson as json
+except:
+    import json
+
+try:
+    from nucosCR import random, hexdigest_n
+    isCR = True
+except:
+    import random
+    isCR = False
+
+from nucosObs import loop, debug
+from nucosObs.observer import broadcast
+
+
+class AiohttpWebsocketInterface(object):
+    def __init__(self, app, broker, doAuth=False, closeOnClientQuit=False, authenticator=None, sslClient=None, sslServer=None):
+        """
+        NOTE: authenticator must have a method: startAuth(msg, wsi)
+        """
+        self.app = app
+        self.ws = {}
+        self.doAuth = doAuth
+        self.broker = broker
+        self.server = None
+        self.authenticator = authenticator
+        self.nonce = {}
+        self.isAuthenticated = {}
+        self.connectedUser = {}
+        self.isRefused = []
+        self.closeOnClientQuit = closeOnClientQuit
+        self.sslClient = sslClient
+        self.sslServer = sslServer
+        self.approved = []
+        app.router.add_route('GET', '/ws', self.handler)
+
+    async def send(self, msg, user):
+        id_ = self.connectedUser.get(user)
+        if id_ is None:
+            return
+        else:
+            try:
+                await self.ws[id_].send_str(msg)
+            except:
+                pass
+
+    async def broadcast(self, msg):
+        for id_, ws in self.ws.items():
+            try:
+                await ws.send_str(msg)
+            except:
+                pass
+
+    async def connect(self, host, port):
+        if debug[-1]:
+            print("try to start client")
+        # self.server = await websockets.connect(self.handler, ip, port)
+        if self.sslClient:
+            protocol = "wss"
+        else:
+            protocol = "ws"
+        websocket = await websockets.connect('%s://%s:%s' %(protocol, host, str(port)), ssl=self.sslClient)
+        self.ws['client'] = websocket
+        await self.listener(websocket, 'client')
+
+
+    async def handler(self, request):
+        ws = web.WebSocketResponse()
+        await ws.prepare(request)
+        id_ =  ws.headers.get("Sec-Websocket-Accept")
+        self.ws.update({id_: ws})
+        if debug[-1]:
+            print("Partner connected")     
+            print(self.ws)   
+        if self.doAuth:
+            if isCR:
+                self.nonce[id_] = random(24).decode()
+            else:
+                self.nonce[id_] = bytes([random.getrandbits(4) for i in range(24)])
+            context = {"name": "doAuth",
+                       "args": {"nonce": self.nonce[id_], "id": id_},
+                       "action": "authenticate"}
+            await ws.send_str(json.dumps(context)) #or send_bytes ??
+        await self.listener(ws, id_)
+        # NOTE next line is mandatory for preventing a closed websocket to raise exception
+        return ws
+
+    async def shutdown(self):
+        if debug[-1]:
+            print("in shutdown process ...")
+        await broadcast.put({"name": "broadcast", "args": [{"action": "stop_observer"}]})
+        if self.server is not None:
+            for k in [x for x in self.ws.keys()]:
+                await self.ws[k].close()
+
+    async def listener(self, ws, id_):
+        user = "unknown"
+
+        async for msg in ws:
+            if msg.type == aiohttp.WSMsgType.text:
+                data = msg.data
+                if not id_ in self.isAuthenticated and self.doAuth:
+                    id_out, user = await self.authenticator.startAuth(data, ws, self.nonce[id_])
+                    if id_out is not None and id_out == id_:
+                        self.isAuthenticated.update({id_: user})
+                        if user in self.connectedUser:
+                            id_old = self.connectedUser.pop(user)
+                            self.isAuthenticated.pop(id_old)
+                            try:
+                                await self.ws[id_old].close()
+                            except:
+                                pass
+                            finally:
+                                if debug[-1]:
+                                    print("closed one pending connection of user %s" % user)
+                            self.connectedUser.update({user : id_})
+                        else:
+                            self.connectedUser.update({user : id_})
+                        # print(self.connectedUser, self.ws)
+                    else:
+                        self.ws.pop(id_)
+                        ws.close()
+                        break
+                else:
+                    await self.broker.put(data)
+        if id_ == "client":
+            await self.shutdown()
+        else:
+            self.ws.pop(id_)
+            if self.closeOnClientQuit:
+                if debug[-1]:
+                    print("client died ...")    
+                if len(self.ws) == 0:
+                    await self.broker.put("client exit")
+                    await self.shutdown()
+            if id_ in self.isAuthenticated:
+                user = self.isAuthenticated.pop(id_)
+                if user in self.connectedUser:
+                    self.connectedUser.pop(user)
+                if debug[-1]:
+                    print("after client left:")
+                    print("user...",self.connectedUser)
+                    print("ws.....",self.ws)
+                
+
+
+        if debug[-1]:
+            print("--- connection of %s stopped " % user)
